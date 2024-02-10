330d6d15d917d8dd829432f71b8bc931d4c6d6c8
Melike İsbir <melikeisbr@gmail.com>
2024-02-10 20:49:11 +0300

    controller

---
+++ ChainOfResponsibilityDesignPattern/DesignPattern.ChainOfResponsibility/Controllers/DefaultController.cs
@@ -0,0 +1,30 @@
+﻿using DesignPattern.ChainOfResponsibility.ChainOfResponsibility;
+using DesignPattern.ChainOfResponsibility.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesignPattern.ChainOfResponsibility.Controllers
+{
+    public class DefaultController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(CustomerProcessViewModel model)
+        {
+            Employee treasurer = new Treasurer();
+            Employee managerAssistant = new ManagerAssistant();
+            Employee manager = new Manager();
+            Employee areaDirector = new AreaDirector();
+
+            treasurer.SetNextApprover(managerAssistant);
+            managerAssistant.SetNextApprover(manager);
+            manager.SetNextApprover(areaDirector);
+
+            treasurer.ProcessRequest(model); //treasurer üzerinde modelden gelen değerle istek başlat
+            return View();
+        }
+    }
+}
