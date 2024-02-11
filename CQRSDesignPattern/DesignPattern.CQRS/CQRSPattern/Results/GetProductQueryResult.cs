8bbe0e071efda39e16c65653e4fff24b979ba13e
Melike İsbir <melikeisbr@gmail.com>
2024-02-11 19:16:26 +0300

    the addition of packages, migration, result handles classes

---
+++ CQRSDesignPattern/DesignPattern.CQRS/CQRSPattern/Results/GetProductQueryResult.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPattern.CQRS.CQRSPattern.Results
+{
+    public class GetProductQueryResult //Çıktının karşılığı olan propertyleri tutuyor.
+    {
+        public int ID { get; set; }
+        public string ProductName { get; set; } //entitydeki isimle aynı olmak zorunda değil
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+    }
+}
