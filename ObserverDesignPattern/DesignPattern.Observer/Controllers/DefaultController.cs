b3204d3a38252756db4977a1a7c2c622ba2a765e
Melike İsbir <melikeisbr@gmail.com>
2024-02-16 20:30:36 +0300

    register

---
+++ ObserverDesignPattern/DesignPattern.Observer/Controllers/DefaultController.cs
@@ -0,0 +1,37 @@
+﻿using DesignPattern.Observer.DAL;
+using DesignPattern.Observer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Observer.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public DefaultController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(RegisterViewModel model)
+        {
+            var appUser = new AppUser()
+            {
+                Name = model.Name,
+                Surname = model.Surname,
+                Email = model.Email,
+                UserName = model.Username
+            };
+            await _userManager.CreateAsync(appUser, model.Password);
+            return View();
+        }
+    }
+}
--- ObserverDesignPattern/DesignPattern.Observer/Startup.cs
+++ ObserverDesignPattern/DesignPattern.Observer/Startup.cs
@@ -1,3 +1,4 @@
+using DesignPattern.Observer.DAL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -23,6 +24,8 @@ namespace DesignPattern.Observer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<Context>();
+            services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<Context>();
             services.AddControllersWithViews();
         }
 
