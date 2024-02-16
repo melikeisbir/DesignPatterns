1aca02775a67e246cb57aa4a45381fb4a04d622e
Melike İsbir <melikeisbr@gmail.com>
2024-02-16 19:45:13 +0300

    migration, IObserver and ObserverObject

--- ObserverDesignPattern/DesignPattern.Observer/DAL/Context.cs
+++ ObserverDesignPattern/DesignPattern.Observer/DAL/Context.cs
@@ -1,8 +1,16 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace DesignPattern.Observer.DAL
 {
-    public class Context : IdentityDbContext
+    public class Context : IdentityDbContext<AppUser,AppRole,int>
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=MELI\\SQLEXPRESS;initial catalog=DesignPattern4;integrated security=true;");
+        }
+        public DbSet<WelcomeMessage> WelcomeMessages { get; set; }
+        public DbSet<Discount> Discounts { get; set; }
+        public DbSet<UserProcess> UserProcesses { get; set; }
     }
 }
---
+++ ObserverDesignPattern/DesignPattern.Observer/ObserverPattern/ObserverObject.cs
@@ -0,0 +1,30 @@
+﻿using DesignPattern.Observer.DAL;
+using System.Collections.Generic;
+
+namespace DesignPattern.Observer.ObserverPattern
+{
+    public class ObserverObject
+    {
+        private readonly List<IObserver> _observers;
+
+        public ObserverObject()
+        {
+            _observers = new List<IObserver>();
+        }
+        public void RegisterObserver(IObserver observer)
+        {
+            _observers.Add(observer);
+        }
+        public void RemoveObserver(IObserver observer)
+        {
+            _observers.Remove(observer);
+        }
+        public void NotifyObserver(AppUser appUser) //olayları asıl tetikleyecek kısım
+        {
+            _observers.ForEach(x =>
+            {
+                x.CreateNewUser(appUser); //yeni kullanıcı için listenin bütün ögelerini tek tek dolaş
+            });
+        }
+    }
+}
