47c2047fcb598f1c7fc1d266a6b068bf1ca9b747
Melike İsbir <melikeisbr@gmail.com>
2024-02-16 20:17:10 +0300

    the writing of classes

---
+++ ObserverDesignPattern/DesignPattern.Observer/ObserverPattern/CreateMagazineAnnouncement.cs
@@ -0,0 +1,28 @@
+﻿using DesignPattern.Observer.DAL;
+using System;
+
+namespace DesignPattern.Observer.ObserverPattern
+{
+    public class CreateMagazineAnnouncement : IObserver
+    {
+
+        private readonly IServiceProvider serviceProvider;
+        Context context = new Context();
+
+        public CreateMagazineAnnouncement(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+
+        public void CreateNewUser(AppUser appUser)
+        {
+            context.UserProcesses.Add(new UserProcess
+            {
+                NameSurname = appUser.Name + "" + appUser.Surname,
+                Magazine = "Bilim Dergisi",
+                Content = "Bilim dergilerimizin mart sayısı 1 Martta evinize ulaştırılacaktır. Konular Jupiter gezegeni ve Mars olacaktır."
+            });
+            context.SaveChanges();
+        }
+    }
+}
---
+++ ObserverDesignPattern/DesignPattern.Observer/ObserverPattern/CreateWelcomeMessage.cs
@@ -0,0 +1,26 @@
+﻿using DesignPattern.Observer.DAL;
+using System;
+
+namespace DesignPattern.Observer.ObserverPattern
+{
+    public class CreateWelcomeMessage : IObserver
+    {
+        private readonly IServiceProvider serviceProvider;
+        Context context = new Context();
+
+        public CreateWelcomeMessage(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+
+        public void CreateNewUser(AppUser appUser) ///yeni kullanıcı oluşturulduğunda tetiklenecek metot
+        {
+            context.WelcomeMessages.Add(new WelcomeMessage
+            {
+                NameSurname = appUser.Name + "" + appUser.Surname,
+                Content="Dergi bültenimize kayıt olduğunuz için teşekkürler. Dergilerimize web sitemizden ulaşabilirsiniz."
+            });
+            context.SaveChanges();
+        }
+    }
+}
