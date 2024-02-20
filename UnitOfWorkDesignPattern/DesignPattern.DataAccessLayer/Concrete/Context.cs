aee34b73054855bef8639f26d52e3d105d26cea0
Melike İsbir <melikeisbr@gmail.com>
2024-02-20 20:21:26 +0300

    update

---
+++ UnitOfWorkDesignPattern/DesignPattern.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,22 @@
+﻿using DesignPattern.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=MELI\\SQLEXPRESS;initial catalog=DesignPattern5;integrated security=true;");
+        }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Process> Processes { get; set; }
+    }
+}
