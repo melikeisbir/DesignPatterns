884a68b46ce3e8832f41a06ed5f134813026ced0
Melike İsbir <melikeisbr@gmail.com>
2024-02-20 20:56:12 +0300

    interfaces, genericrepository and efdal classes

---
+++ UnitOfWorkDesignPattern/DesignPattern.DataAccessLayer/EntityFramework/EfCustomerDal.cs
@@ -0,0 +1,21 @@
+﻿using DesignPattern.DataAccessLayer.Abstract;
+using DesignPattern.DataAccessLayer.Concrete;
+using DesignPattern.DataAccessLayer.Repositories;
+using DesignPattern.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.DataAccessLayer.EntityFramework
+{
+    public class EfCustomerDal : GenericRepository<Customer>, ICustomerDal
+    {
+        public EfCustomerDal(Context context) : base(context)
+        {
+
+        }
+    }
+}
