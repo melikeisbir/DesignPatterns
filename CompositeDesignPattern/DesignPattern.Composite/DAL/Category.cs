8e9565388e0baeb1093ba9bdc20bd431f7f8f5f2
Melike İsbir <melikeisbr@gmail.com>
2024-02-23 14:28:22 +0300

    composite design pattern, installation of packages, entities and project references, migration

---
+++ CompositeDesignPattern/DesignPattern.Composite/DAL/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPattern.Composite.DAL
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public int UpperCategoryID { get; set; }//bir üstteki category idsi
+        public List<Product> Products { get; set; }
+    }
+}
---
+++ CompositeDesignPattern/DesignPattern.Composite/DAL/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesignPattern.Composite.DAL
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=MELI\\SQLEXPRESS;initial catalog=DesignPattern7;integrated security=true;");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
