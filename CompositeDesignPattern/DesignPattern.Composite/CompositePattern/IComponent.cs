f629382ca6d51dd84c8f2081d1f8557355c14cb9
Melike İsbir <melikeisbr@gmail.com>
2024-02-23 14:50:49 +0300

    IComponent and ProductComponent, ProductComposite class

---
+++ CompositeDesignPattern/DesignPattern.Composite/CompositePattern/IComponent.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPattern.Composite.CompositePattern
+{
+    public interface IComponent
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        int TotalCount();
+        string Display(); //sidebarı oluşturacak olan html kodları barındıracak
+    }
+}
---
+++ CompositeDesignPattern/DesignPattern.Composite/CompositePattern/ProductComposite.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace DesignPattern.Composite.CompositePattern
+{
+    public class ProductComposite : IComponent
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        private List<IComponent> _components;
+
+        public ProductComposite(int id, string name)
+        {
+            Id = id;
+            Name = name;
+            _components = new List<IComponent>();
+        }
+
+        public ICollection<IComponent> Components => _components; //diğer taraflardan erişebileceğimiz koleksiyon üyesi
+
+        public void Add(IComponent component)
+        {
+            _components.Add(component);
+        }
+        public string Display()
+        {
+            var stringBuilder = new StringBuilder();
+            stringBuilder.Append($"<div class='text-success'>{Name} ({TotalCount()})</div>"); //uygula
+            stringBuilder.Append("<ul class='list-group list-group-flush ms-2'>");
+            foreach (var item in _components)
+            {
+                stringBuilder.Append(item.Display()); //displayı her alt categoryde uygula
+            }
+            stringBuilder.Append("</ul>"); //category sonlarında ul ile kapat
+            return stringBuilder.ToString();
+        }
+
+        public int TotalCount()
+        {
+            return _components.Sum(x => x.TotalCount()); //toplam
+        }
+    }
+}
