a11aa08bd51bee134b191b69ad8b77542676657b
Melike İsbir <melikeisbr@gmail.com>
2024-02-24 14:22:14 +0300

    mediator design pattern, installation of packages, entities, migration

---
+++ MediatorDesignPattern/DesignPattern.Mediator/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesignPattern.Mediator.DAL
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=MELI\\SQLEXPRESS;initial catalog=DesignPattern8;integrated security=true;");
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
