7cd46f33ebd835f59c36936593825d6be04ba425
Melike İsbir <melikeisbr@gmail.com>
2024-02-24 14:39:35 +0300

    result, query and handler classes

---
+++ MediatorDesignPattern/DesignPattern.Mediator/MediatorPattern/Queries/GetAllProductQuery.cs
@@ -0,0 +1,9 @@
+﻿using DesignPattern.Mediator.MediatorPattern.Results;
+using MediatR;
+
+namespace DesignPattern.Mediator.MediatorPattern.Queries
+{
+    public class GetAllProductQuery : IRequest<List<GetAllProductQueryResult>> //çıktısı isteniyor
+    {
+    }
+}
