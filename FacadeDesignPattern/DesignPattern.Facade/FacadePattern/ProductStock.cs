b902014c97db71067123dbd241ac63006b03fc4f
Melike İsbir <melikeisbr@gmail.com>
2024-02-26 20:36:51 +0300

    OrderDetail and ProductStock classes

---
+++ FacadeDesignPattern/DesignPattern.Facade/FacadePattern/ProductStock.cs
@@ -0,0 +1,15 @@
+﻿using DesignPattern.Facade.DAL;
+
+namespace DesignPattern.Facade.FacadePattern
+{
+    public class ProductStock
+    {
+        Context context = new Context();
+        public void StockDecrease(int id, int amount) //stok azaltma işlemi için
+        {
+            var value = context.Products.Find(id);
+            value.ProductStock -= amount;
+            context.SaveChanges();
+        }
+    }
+}
