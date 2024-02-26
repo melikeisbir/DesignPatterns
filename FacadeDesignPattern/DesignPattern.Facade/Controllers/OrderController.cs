e46838b793ea9fb131f0fd330e9a1a7124bfdf90
Melike İsbir <melikeisbr@gmail.com>
2024-02-26 21:59:04 +0300

    order operations

---
+++ FacadeDesignPattern/DesignPattern.Facade/Controllers/OrderController.cs
@@ -0,0 +1,27 @@
+﻿using DesignPattern.Facade.DAL;
+using DesignPattern.Facade.FacadePattern;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesignPattern.Facade.Controllers
+{
+    public class OrderController : Controller
+    {
+        Context context = new Context();
+        [HttpGet]
+        public IActionResult OrderStart()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult OrderStart(int customerID, int productId, int orderID, int productCount, decimal productPrice)
+        {
+            OrderFacade order = new OrderFacade();
+            order.CompleteOrder(customerID, productId, orderID, productCount, productPrice);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
