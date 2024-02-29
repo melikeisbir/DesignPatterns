4895daff7b75bdae1508b328f23edf70a8ca7b7d
Melike İsbir <melikeisbr@gmail.com>
2024-02-29 19:48:42 +0300

    encrypt by content decorator

---
+++ DecoratorDesignPattern/DesignPattern.Decorator/DecoratorPattern2/EncryptByContentDecorator.cs
@@ -0,0 +1,36 @@
+﻿using DesignPattern.Decorator.DAL;
+using System;
+
+namespace DesignPattern.Decorator.DecoratorPattern2
+{
+    public class EncryptByContentDecorator : Decorator //içeriği şifreleme
+    {
+        private readonly ISendMessage _sendMessage;
+        Context context = new Context();
+        public EncryptByContentDecorator(ISendMessage sendMessage) : base(sendMessage)
+        {
+            _sendMessage = sendMessage;
+        }
+        public void SendMessageByEncryptContent(Message message)
+        {
+            message.MessageSender = "Takım Lideri";
+            message.MessageReceiver = "Yazılım Ekibi";
+            message.MessageContent = "Saat 17.00'da Publish yapılacak";
+            message.MessageSubject = "Publish";
+            string data = "";
+            data = message.MessageContent;
+            char[] chars = data.ToCharArray(); //karakterleri arraya dönüştürme
+            foreach (var item in chars)
+            {
+                message.MessageContent += Convert.ToChar(item + 3).ToString(); //chara dönüştür itemdan gelen değerin üzerine 3 karakter ekle ötele
+            }
+            context.Messages.Add(message);
+            context.SaveChanges();
+        }
+        public override void SendMessage(Message message)
+        {
+            base.SendMessage(message);
+            SendMessageByEncryptContent(message);
+        }
+    }
+}
