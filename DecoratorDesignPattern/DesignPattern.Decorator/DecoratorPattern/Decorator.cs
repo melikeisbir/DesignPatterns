16fd73b0d84c5994f8e509d4b5ce3d89dd1674e6
Melike İsbir <melikeisbr@gmail.com>
2024-02-29 19:13:37 +0300

    decorator class

---
+++ DecoratorDesignPattern/DesignPattern.Decorator/DecoratorPattern/Decorator.cs
@@ -0,0 +1,23 @@
+﻿using DesignPattern.Decorator.DAL;
+
+namespace DesignPattern.Decorator.DecoratorPattern
+{
+    public class Decorator : INotifier //default olarak bildirim oluştursun
+    {
+        private readonly INotifier _notifier;
+
+        public Decorator(INotifier notifier)
+        {
+            _notifier = notifier;
+        }
+
+        public void CreateNotify(Notifier notifier) // içinde context sınıfı oldugu için bir daha tanımlamadık
+        {
+            notifier.NotifierCreator = "Admin";
+            notifier.NotifierSubject = "Toplantı";
+            notifier.NotifierType = "Public";
+            notifier.NotifierChannel = "Whatsapp";
+            _notifier.CreateNotify(notifier);
+        }
+    }
+}
